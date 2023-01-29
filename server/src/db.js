f7b033451bffd3baafe3c77104f9aa7ed12779a9
JuampaVLB <juampablovillalba25@gmail.com>
2023-01-29 03:43:09 -0300

    first commit

---
+++ server/src/db.js
@@ -0,0 +1,16 @@
+import { connect } from "mongoose";
+import { MONGODB_URI } from './config.js';
+
+export const connectDB = async () => {
+    try {
+        
+        await connect(MONGODB_URI);
+        
+        console.log('connect to db');
+    
+    } catch(err) {
+      
+        console.log(err);
+    
+    }
+};
